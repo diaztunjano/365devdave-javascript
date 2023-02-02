f5943782de494a64920708b2552e408022e20f87
David <david.diaz.tunjano@gmail.com>
2023-02-01 21:39:37 -0300

    feat: border case to lose game

--- 5 - Guess Number/script.js
+++ 5 - Guess Number/script.js
@@ -5,18 +5,23 @@ let score = 20;
 
 document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
-  if (!guess) {
-    document.querySelector(".message").textContent = "No number 🚫";
-  }
-  if (guess < randomNumber) {
-    document.querySelector(".message").textContent = "To low 🥱 ";
-    score -= 1;
-    document.querySelector(".score").textContent = score;
-  } else if (guess > randomNumber) {
-    document.querySelector(".message").textContent = "To high 🫣 ";
-    score -= 1;
-    document.querySelector(".score").textContent = score;
-  } else if (guess === randomNumber) {
-    document.querySelector(".message").textContent = "Correct 🤑 ";
+
+  if (score > 0) {
+    if (!guess) {
+      document.querySelector(".message").textContent = "No number 🚫";
+    }
+    if (guess < randomNumber) {
+      document.querySelector(".message").textContent = "To low 🥱 ";
+      score -= 1;
+      document.querySelector(".score").textContent = score;
+    } else if (guess > randomNumber) {
+      document.querySelector(".message").textContent = "To high 🫣 ";
+      score -= 1;
+      document.querySelector(".score").textContent = score;
+    } else if (guess === randomNumber) {
+      document.querySelector(".message").textContent = "Correct 🤑 ";
+    }
+  } else {
+    document.querySelector(".message").textContent = "You lost the game 💩 ";
   }
 });
