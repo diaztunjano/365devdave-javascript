1a79f754622499eb0047ee61bc8097d3153c9aec
David <david.diaz.tunjano@gmail.com>
2023-02-01 21:36:49 -0300

    feat: reducing score

--- 5 - Guess Number/script.js
+++ 5 - Guess Number/script.js
@@ -1,8 +1,22 @@
 "use strict";
 
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", () => {
-  const guess = document.querySelector(".guess").value;
+  const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number 🚫";
+  }
+  if (guess < randomNumber) {
+    document.querySelector(".message").textContent = "To low 🥱 ";
+    score -= 1;
+    document.querySelector(".score").textContent = score;
+  } else if (guess > randomNumber) {
+    document.querySelector(".message").textContent = "To high 🫣 ";
+    score -= 1;
+    document.querySelector(".score").textContent = score;
+  } else if (guess === randomNumber) {
+    document.querySelector(".message").textContent = "Correct 🤑 ";
   }
 });
