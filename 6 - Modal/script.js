e9d779f56e3a8bc6eb9ccc457a5d99ccec51d8dd
David <david.diaz.tunjano@gmail.com>
2023-02-02 18:48:08 -0300

    feat: add classes manipulation

--- 6 - Modal/script.js
+++ 6 - Modal/script.js
@@ -3,3 +3,11 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".close-modal");
+const btnOpen = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnOpen.length; i++) {
+  const btn = btnOpen[i];
+  btn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+}
