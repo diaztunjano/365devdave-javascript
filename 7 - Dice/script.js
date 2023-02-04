d3de70e8c5c0578e6b6b142b22b8b234f665e3db
David <david.diaz.tunjano@gmail.com>
2023-02-04 14:20:21 -0300

    feat: change background color

--- 7 - Dice/script.js
+++ 7 - Dice/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const player0El = document.querySelector(`.player--0`);
+const player1El = document.querySelector(`.player--1`);
+
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
@@ -13,8 +16,6 @@ const scores = [0, 0];
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-
-console.log(diceEl);
 
 diceEl.classList.add("hidden");
 
@@ -39,5 +40,7 @@ btnRoll.addEventListener("click", () => {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
