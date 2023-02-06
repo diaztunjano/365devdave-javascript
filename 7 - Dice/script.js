0b97ea5c1895fe204e5363a0c65097c60beb43c3
David <david.diaz.tunjano@gmail.com>
2023-02-05 22:28:27 -0300

    fix: add spaces

--- 7 - Dice/script.js
+++ 7 - Dice/script.js
@@ -5,7 +5,9 @@ const player1El = document.querySelector(`.player--1`);
 
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+
 const diceEl = document.querySelector(".dice");
+
 const currentScoreP0El = document.getElementById("current--0");
 const currentScoreP1El = document.getElementById("current--1");
 
