0bcff0a025004142b1ec3f0e1dedfcedac6b047c
David <david.diaz.tunjano@gmail.com>
2023-02-06 21:40:55 -0300

    feat: this initial

--- 8 - JS Theory/script.js
+++ 8 - JS Theory/script.js
@@ -2,48 +2,52 @@
 
 // Hoisting:
 
-// console.log(me);
-// console.log(job);
-// console.log(year);
+// // console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-// var me = "dave";
-// let job = "dev";
-// const year = 1991;
+// // var me = "dave";
+// // let job = "dev";
+// // const year = 1991;
 
-// --------
-// console.log(add(7, 8));
+// // --------
+// // console.log(add(7, 8));
 
-// This function is HOISTED, so I
-// can log the result before its initialization
-function add(a, b) {
-  return a + b;
-}
+// // This function is HOISTED, so I
+// // can log the result before its initialization
+// function add(a, b) {
+//   return a + b;
+// }
 
-// --------
-// Doesn't work here. Needs to be executed after addExpr initialization.
-// console.log(addExpr(10, 12));
+// // --------
+// // Doesn't work here. Needs to be executed after addExpr initialization.
+// // console.log(addExpr(10, 12));
 
-// Const function is unitialized -> it is in the temporal dead zone.
-const addExpr = function (a, b) {
-  return a + b;
-};
+// // Const function is unitialized -> it is in the temporal dead zone.
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
 
-// Here it works:
-// console.log(addExpr(10, 12));
+// // Here it works:
+// // console.log(addExpr(10, 12));
 
-// --------
+// // --------
 
-const addArrow = (a, b) => a + b;
+// const addArrow = (a, b) => a + b;
 
-// DONT USE VAR:
+// // DONT USE VAR:
 
-if (!numProducts) {
-  console.log("Delete Everything! HEHE");
-}
+// if (!numProducts) {
+//   console.log("Delete Everything! HEHE");
+// }
 
-var numProducts = 10;
+// var numProducts = 10;
 
-// DONT USE VAR!
-// "Delete Everything! HEHE" Printed.
-// That is beacause before initialization, numProducts
-// is undefined because it is "var". So !undefined === true
+// // DONT USE VAR!
+// // "Delete Everything! HEHE" Printed.
+// // That is beacause before initialization, numProducts
+// // is undefined because it is "var". So !undefined === true
+
+// ------------------------
+
+// THIS KEYWORD
