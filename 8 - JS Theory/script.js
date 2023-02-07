c015ed4b58186841f4a886fb16f317340b14db36
David <david.diaz.tunjano@gmail.com>
2023-02-06 21:21:52 -0300

    feat: 3 examples

--- 8 - JS Theory/script.js
+++ 8 - JS Theory/script.js
@@ -9,3 +9,28 @@
 // var me = "dave";
 // let job = "dev";
 // const year = 1991;
+
+// --------
+// console.log(add(7, 8));
+
+// This function is HOISTED, so I
+// can log the result before its initialization
+function add(a, b) {
+  return a + b;
+}
+
+// --------
+// Doesn't work here. Needs to be executed after addExpr initialization.
+// console.log(addExpr(10, 12));
+
+// Const function is unitialized -> it is in the temporal dead zone.
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+// Here it works:
+// console.log(addExpr(10, 12));
+
+// --------
+
+const addArrow = (a, b) => a + b;
