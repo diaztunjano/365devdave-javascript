b779bbcdd188775ded1c1d213f5ceda665bf5148
David <david.diaz.tunjano@gmail.com>
2023-02-08 09:52:26 -0300

    feat: destructuring objects 1

--- 9 - Data Structure/script.js
+++ 9 - Data Structure/script.js
@@ -32,6 +32,8 @@ const restaurant = {
   },
 };
 
+// --------------------- DESTRUCTURE ARRAYS:
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -56,12 +58,47 @@ secondary = temp;
 // console.log(main, secondary);
 
 const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// console.log(starter, mainCourse);
 
 // Nested arrays:
 const nested = [2, 4, [5, 6]];
 const [i, , j] = nested;
-console.log(i, j); // [2, [5,6]]
+// console.log(i, j); // [2, [5,6]]
 // If we want to destructure the nested array:
 const [l, , [m, n]] = nested;
-console.log(l, m, n); // 2 5 6
+// console.log(l, m, n); // 2 5 6
+
+// --------------------- DESTRUCTURE OBJECTS:
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
