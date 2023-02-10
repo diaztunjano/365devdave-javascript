cbc8d2806aae5d4789257a7a77394c83e1729924
David <david.diaz.tunjano@gmail.com>
2023-02-10 18:56:58 -0300

    feat: challenge

--- 9 - Data Structure/script.js
+++ 9 - Data Structure/script.js
@@ -224,33 +224,136 @@ const operators = 4 || "dave";
 
 // _-------------------
 
-const rest1 = {
-  name: "Capri",
-  numGuests: 0,
-};
+// const rest1 = {
+//   name: "Capri",
+//   numGuests: 0,
+// };
 
-const rest2 = {
-  name: "Capri",
-  owner: "Giorgio",
-};
+// const rest2 = {
+//   name: "Capri",
+//   owner: "Giorgio",
+// };
 
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
 
-// OR ASSIGNMENT OPERATOR
-// SAME AS:
-// rest1.numGuests ||= 10; // Assigns value to a variable if variable is FALSY
-// rest2.numGuests ||= 10;
-
-// NULLISH ASSIGNMENT OPERATOR
+// // OR ASSIGNMENT OPERATOR
 // // SAME AS:
-// rest1.numGuests ??= 10; // Assigns value to a variable if variable is FALSY
-// rest2.numGuests ??= 10;
+// // rest1.numGuests ||= 10; // Assigns value to a variable if variable is FALSY
+// // rest2.numGuests ||= 10;
 
-// AND ASSIGNMENT OPERATOR
+// // NULLISH ASSIGNMENT OPERATOR
+// // // SAME AS:
+// // rest1.numGuests ??= 10; // Assigns value to a variable if variable is FALSY
+// // rest2.numGuests ??= 10;
 
-rest1.owner = rest1.owner && "<ANONYMOUS>";
-rest2.owner = rest2.owner && "<ANONYMOUS>";
+// // AND ASSIGNMENT OPERATOR
 
-console.log(rest1);
-console.log(rest2);
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+
+// console.log(rest1);
+// console.log(rest2);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players. 
+For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array 
+('fieldPlayers') with all the remaining 10 field players
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') 
+containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and 
+prints each of them to the console, along with the number of goals that were scored in total 
+(number of player names passed in)
+
+7. The team with the lower odd is more likely to win. 
+Print to the console which team is more likely to win,
+ WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 
+'Lewandowski' and 'Kimmich'. Then, call the function again 
+with players from game.scored
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+  printGoals: function (...playerNames) {
+    console.log(`GOALS: ${this.score} - PLAYERS: ${playerNames}`);
+  },
+};
+
+// TASK 1:
+const [players1, players2] = game.players;
+
+// TASK 2:
+const [gk, ...fieldPlayers] = players1;
+
+// TASK 3:
+const allPlayers = [...players1, ...players2];
+
+// TASK 4:
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+// TASK 5:
+const { team1: oddTeam1, x: oddDraw, team2: oddTeam2 } = game.odds;
+
+//TASK 6:
+game.printGoals(game.scored);
+
+// TASK 7 :
+// console.log(
+//   `Most likely to win: ${oddTeam1 < oddTeam2 ? game.team1 : game.team2}`
+// );
+
+oddTeam1 < oddTeam2 && console.log(`- Most likely to win: ${game.team1}`);
+oddTeam1 > oddTeam2 && console.log(`- Most likely to win: ${game.team2}`);
