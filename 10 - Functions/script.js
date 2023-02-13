a4817fcd87b2fee244e693486d1e8532566ce12f
David <david.diaz.tunjano@gmail.com>
2023-02-13 18:27:42 -0300

    feat: functions initial value

---
+++ 10 - Functions/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const bookings = [];
+
+// const createBooking = (flightNum, numPass = 199, price = 500) => {
+//   const booking = {
+//     flightNum,
+//     numPass,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking("LHE123");
+
+// 2 -----
