79a2fa3b1590f1d85ee5cb11a89ae954b516185d
David <david.diaz.tunjano@gmail.com>
2023-02-13 18:41:36 -0300

    feat: passing by value functions

--- 10 - Functions/script.js
+++ 10 - Functions/script.js
@@ -15,3 +15,26 @@
 // createBooking("LHE123");
 
 // 2 -----
+
+const flight = "LHE123";
+
+const jonas = {
+  name: "Jonas Pepe",
+  passport: 12345,
+};
+
+const checkFlight = (flightNum, passenger) => {
+  flightNum = "LJJ321";
+  passenger.name = "El" + passenger.name;
+  if (passenger.passport === 12345) {
+    alert("Correct! ");
+  } else {
+    alert("Wrong passenger");
+  }
+};
+
+checkFlight(flight, jonas);
+console.log(flight); // Doesn't get modified. Still is LHE 123
+console.log(jonas); // Gets its name changed. Because, in 2nd line,
+// I change the name. 'jonas' and passenger.name were referencing
+// the same object in the HEAP. So if one changes it, the other will see it too
