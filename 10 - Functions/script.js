39ea6d9a065773e8a27d6576689836ab4624915e
David <david.diaz.tunjano@gmail.com>
2023-02-14 11:31:13 -0300

    fix: add correct formatting

--- 10 - Functions/script.js
+++ 10 - Functions/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// 1 --------------------- PASSING ARGUMENTS
+
 // const bookings = [];
 
 // const createBooking = (flightNum, numPass = 199, price = 500) => {
@@ -14,7 +16,7 @@
 
 // createBooking("LHE123");
 
-// 2 -----
+// 2 --------------
 
 // const flight = "LHE123";
 
@@ -39,7 +41,7 @@
 // // I change the name. 'jonas' and passenger.name were referencing
 // // the same object in the HEAP. So if one changes it, the other will see it too
 
-// 3 -----------
+// 3 ----------- FIRST CLASS AND HIGHER ORDER FUNCTIONS: CALLBACK FUNCTIONS
 
 // const oneWord = (str) => {
 //   return str.replace(/ /g, "").toLowerCase();
@@ -58,7 +60,7 @@
 // transformer("Javascript is the best!", upperFirstWord); //Javascript is the best!
 // transformer("Javascript is the best!", oneWord); // javascriptisthebest!
 
-// 4 ------------
+// 4 ------------ FUNCTIONS THAT RETURN FUNCTIONS
 
 // const greet = (greeting) => {
 //   (name) => {
@@ -75,49 +77,49 @@
 // // The same as:
 // greet("Hello")("Jonas");
 
-// 5 ----------------
+// 5 ---------------- CALL AND APPLY KEYWORDS
 
-const lan = {
-  airline: "LATAM",
-  iataCode: "LH",
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lan = {
+//   airline: "LATAM",
+//   iataCode: "LH",
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-lan.book(239, "Dave");
-console.log(lan);
+// lan.book(239, "Dave");
+// console.log(lan);
 
-const eurowings = {
-  airline: "Eurowings",
-  iataCode: "EW",
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-  },
-};
+// const eurowings = {
+//   airline: "Eurowings",
+//   iataCode: "EW",
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//   },
+// };
 
-// Copying the function to a new variable 'book'
-const book = lan.book;
-// book(23, "Venena"); // ERROR: Can't read airline of undefined
-// This happens because 'book' now is a regular function and the 'this'
-// element now is undefined.
+// // Copying the function to a new variable 'book'
+// const book = lan.book;
+// // book(23, "Venena"); // ERROR: Can't read airline of undefined
+// // This happens because 'book' now is a regular function and the 'this'
+// // element now is undefined.
 
-book.call(eurowings, 23, "Chavo");
-console.log(eurowings);
+// book.call(eurowings, 23, "Chavo");
+// console.log(eurowings);
 
-// apply method:
-const flightData = [543, "George Cooper"];
-book.apply(eurowings, flightData);
-console.log(eurowings);
+// // apply method:
+// const flightData = [543, "George Cooper"];
+// book.apply(eurowings, flightData);
+// console.log(eurowings);
 
-// Better with spread:
-const flightData2 = [599, "Jonas Johnson"];
-book.call(eurowings, ...flightData2);
-console.log(eurowings);
+// // Better with spread:
+// const flightData2 = [599, "Jonas Johnson"];
+// book.call(eurowings, ...flightData2);
+// console.log(eurowings);
