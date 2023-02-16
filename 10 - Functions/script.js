a5ee7afb04f46765d18cc3f20a0ff15c53e1f024
David <david.diaz.tunjano@gmail.com>
2023-02-15 22:42:40 -0300

    feat: challenge

--- 10 - Functions/script.js
+++ 10 - Functions/script.js
@@ -94,16 +94,16 @@
 // lan.book(239, "Dave");
 // console.log(lan);
 
-const eurowings = {
-  airline: "Eurowings",
-  iataCode: "EW",
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-  },
-};
+// const eurowings = {
+//   airline: "Eurowings",
+//   iataCode: "EW",
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//   },
+// };
 
 // // Copying the function to a new variable 'book'
 // const book = lan.book;
@@ -135,27 +135,109 @@ const eurowings = {
 // // Now, we only pass the passenger name
 // bookEWFlight23("Peperreito");
 
-eurowings.planes = 300;
-eurowings.buyPlane = function name(params) {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
+// eurowings.planes = 300;
+// eurowings.buyPlane = function name(params) {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// document
+//   .querySelector(".buy")
+//   .addEventListener("click", eurowings.buyPlane.bind(eurowings));
+
+// const addTax = (rate, value) => value + value * rate;
+// // Using null as the element that will point to use "this" keyword
+// const addVAT = addTax.bind(null, 0.23);
+
+// // console.log(addVAT(100)); // 123
+
+// const VATcalc = function (rate) {
+//   return function (value) {
+//     console.log(value + value * rate);
+//   };
+// };
+
+// const a = VATcalc(0.23);
+// a(100);
+
+// ------------------
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose,
+ and an array with the number of replies for each option. This data is stored 
+ in the starter object below.
+Here are your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. 
+  For example, if the option is 3, increase the value AT POSITION 3 
+  of the array by 1. Make sure to check if the input is a number and if 
+  the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The 
+method takes a string as an input (called 'type'), which can be either
+ 'string' or 'array'. If type is 'array', simply display the results array as 
+ it is, using console.log(). This should be the default option. If type is 'string',
+  display a string like "Poll results are 13, 2, 4, 1". 
+
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: "What is your favorite language?",
+  options: ["0: Javascript", "1: Python", "2: Rust", "3: Go"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const displayMessage = `${this.question}
+        ${this.options[0]}
+        ${this.options[1]}
+        ${this.options[2]}
+        ${this.options[3]}
+        (Write option number)`;
+    const answer = Number(prompt(displayMessage));
+    if ([0, 1, 2, 3].includes(answer)) {
+      this.answers[answer]++;
+      this.displayResults("Array");
+    } else {
+      console.log("Input a valid answer");
+    }
+  },
+  displayResults(type = "string") {
+    if (type === "string") {
+      console.log(
+        `Poll results are ${this.answers[0]}, ${this.answers[1]}, ${this.answers[2]}, ${this.answers[3]}`
+      );
+    } else {
+      console.log(this.answers);
+    }
+  },
 };
-document
-  .querySelector(".buy")
-  .addEventListener("click", eurowings.buyPlane.bind(eurowings));
 
-const addTax = (rate, value) => value + value * rate;
-// Using null as the element that will point to use "this" keyword
-const addVAT = addTax.bind(null, 0.23);
+// bind to functions! not objects.
+// the callback function of addEventListener will point 'this' to the
+// .poll
+const pollEl = poll.displayResults.bind(poll);
 
-// console.log(addVAT(100)); // 123
+document.querySelector(".poll").addEventListener("click", pollEl);
 
-const VATcalc = function (rate) {
-  return function (value) {
-    console.log(value + value * rate);
-  };
-};
-
-const a = VATcalc(0.23);
-a(100);
+poll.registerNewAnswer();
