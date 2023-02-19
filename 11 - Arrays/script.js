cf56f8d33ab601e09eaad948922df0194e00b374
David <david.diaz.tunjano@gmail.com>
2023-02-19 09:36:04 -0300

    fix: css colors

--- 11 - Arrays/script.js
+++ 11 - Arrays/script.js
@@ -63,7 +63,7 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 const displayMovements = (movements) => {
   movements.forEach((mov, i) => {
-    const type = mov > 0 ? "deposit" : "withdraw";
+    const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
