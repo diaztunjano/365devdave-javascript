c9e5a01bf95da15638bcbb662c61d98e977dec9c
David <david.diaz.tunjano@gmail.com>
2023-02-19 09:35:05 -0300

    feat: bankist initial files

--- 11 - Arrays/script.js
+++ 11 - Arrays/script.js
@@ -61,6 +61,26 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = (movements) => {
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? "deposit" : "withdraw";
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    
+    `;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -112,25 +132,29 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 // -------------- 3
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// We pass a callback function to the forEach
-movements.forEach((mov, i) => {
-  if (mov > 0) {
-    console.log(`${i + 1} - Deposited: ${mov}`);
-  } else {
-    console.log(`${i + 1} - Withdrew: ${Math.abs(mov)}`);
-  }
-});
+// // We pass a callback function to the forEach
+// movements.forEach((mov, i) => {
+//   if (mov > 0) {
+//     console.log(`${i + 1} - Deposited: ${mov}`);
+//   } else {
+//     console.log(`${i + 1} - Withdrew: ${Math.abs(mov)}`);
+//   }
+// });
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
+// const currencies = new Map([
+//   ["USD", "United States dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound sterling"],
+// ]);
 
-currencies.forEach((value, key, map) => {
-  console.log(value);
-  console.log(key);
-  console.log(map);
-});
+// currencies.forEach((value, key, map) => {
+//   console.log(value);
+//   console.log(key);
+//   console.log(map);
+// });
+
+// 4 ---------------- Bankist introduction
+
+// 5 ----------
