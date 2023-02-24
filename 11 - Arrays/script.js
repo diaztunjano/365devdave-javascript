e9c46fd0648bf986a238a6908820e856cf273ddb
David <david.diaz.tunjano@gmail.com>
2023-02-24 10:45:12 -0300

    feat : new challenge

--- 11 - Arrays/script.js
+++ 11 - Arrays/script.js
@@ -344,3 +344,28 @@ GOOD LUCK 😀
 //     return euroToUSD * mov;
 //   })
 //   .reduce((acc, mov) => acc + mov, 0);
+
+// CHALLENGE 3
+
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the 
+previous challenge, but this time as an arrow function,
+ and using chaining!
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = (ages) => {
+  const averageAge = ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((dog) => dog <= 18)
+    .reduce((acc, curr, i, arr) => {
+      return (acc + curr) / arr.length;
+    });
+  console.log(`The average age is ${averageAge}`);
+};
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
