11c7eda1af4827c4cba57b8dac59fdcf7a44d78e
David <david.diaz.tunjano@gmail.com>
2023-03-01 09:01:36 -0300

    fix: delete account

--- 11 - Arrays/script.js
+++ 11 - Arrays/script.js
@@ -193,7 +193,18 @@ btnClose.addEventListener("click", (e) => {
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount?.pin
   ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(accounts);
+    //delete User
+    accounts.splice(index, 1);
+    console.log(accounts);
+
+    // HIde ui
+    containerApp.style.opacity = 0;
   }
+  inputCloseUsername.value = inputClosePin.value = "";
 });
 
 /////////////////////////////////////////////////
