c48eb55ff9b8c4c71e98a5e91ed2667e990753f4
David <david.diaz.tunjano@gmail.com>
2023-03-14 13:04:20 -0300

    feat: format current date to title

--- 12 - Dates and Numbers/script.js
+++ 12 - Dates and Numbers/script.js
@@ -81,10 +81,12 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
@@ -142,7 +144,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -164,9 +166,15 @@ containerApp.style.opacity = 100;
 // DATES
 
 const now = new Date();
-labelDate.textContent = now;
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = `${now.getMinutes()}`.padStart(2, 0);
 
-//
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+// Format to day/month/year
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -260,7 +268,7 @@ btnClose.addEventListener("click", function (e) {
 let sorted = false;
 btnSort.addEventListener("click", function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
